c7065b508a9f3e06f0fb57e6d127dd6bf4c25062
rafa <rafaelrgo.icm@gmail.com>
2024-08-19 09:26:18 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // Aqui é criada a classe HelloWorld
+    public static void main(String[] args) { // Aqui é criada a classe main
+        System.out.println("Hello World!!!"); //Aqui é "impressa" um lilha com texto "HelloWorld"
+    } // Aqui encerra p bloco de código do metodo main
+} // Aqui encerra o bloco de codigo da classe HelloWorld
